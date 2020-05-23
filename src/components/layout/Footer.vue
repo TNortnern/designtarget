bba70c6bba734652c63cacc9c582d3b57c935f8f
TNORTHERN <traynorthern@yahoo.com>
2020-05-23 11:07:54 -0500

    footer stuff

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -3,21 +3,35 @@
     :style="`background-color: ${$store.state.black}`"
     class="footer container pa-10"
   >
-    <v-row>
-      <v-col cols="4">
+    <v-row class="text-center text-sm-left" justify="center" justify-sm="start">
+      <v-col cols="12" sm="5" xl="4">
         <h1>designtarget</h1>
         <p class="mt-5">
           designtarget is a curated list of design resources that will help you
           speed up your workflow.
         </p>
       </v-col>
-      <v-col cols="1">
-        <a href="#" class="d-block" v-for="item in 5" :key="item">Link</a>
+      <v-col cols="6" sm="2" xl="2">
+        <router-link
+          :to="link.href"
+          class="d-block"
+          v-for="link in linksOne"
+          :key="link.name"
+        >
+          {{ link.name }}
+        </router-link>
       </v-col>
-      <v-col cols="2">
-        <a href="#" class="d-block" v-for="item in 2" :key="item">Link</a>
+      <v-col cols="6" sm="2" xl="2">
+        <router-link
+          :to="link.href"
+          class="d-block"
+          v-for="link in linksTwo"
+          :key="link.name"
+        >
+          {{ link.name }}
+        </router-link>
       </v-col>
-      <v-col cols="5">
+      <v-col sm="3" xl="3">
         <p>
           Sign up to our newsletter and get notified whenever a new resource is
           released.
@@ -29,7 +43,21 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    linksOne: [
+      { name: "Images", href: "#images" },
+      { name: "Fonts", href: "#fonts" },
+      { name: "Illustrations", href: "#illustrations" },
+      { name: "Videos", href: "#videos" },
+      { name: "Landing Pages", href: "#landing" }
+    ],
+    linksTwo: [
+      { name: "Facebook", href: "#facebook" },
+      { name: "Twitter", href: "#twitter" }
+    ]
+  })
+};
 </script>
 
 <style lang="scss" scoped>
@@ -41,5 +69,18 @@ footer {
 a {
   color: #ffffff !important;
   text-decoration: none;
+  @include xl() {
+    font-size: 24px;
+  }
+}
+h1 {
+  @include xl() {
+    font-size: 50px;
+  }
+}
+p {
+  @include xl() {
+    font-size: 24px;
+  }
 }
 </style>
