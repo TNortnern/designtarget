9442b2e95cb47d7617eefc2de56e1754b845335a
TNORTHERN <traynorthern@yahoo.com>
2020-05-28 11:18:07 -0500

    initial queries

---
+++ src/graphql/queries/users.js
@@ -0,0 +1,13 @@
+import gql from "graphql-tag";
+
+export const LOGIN_QUERY = gql`
+  query($email: String, $password: String) {
+    login(email: $email, password: $password) {
+      user {
+        id
+        email
+      }
+      token
+    }
+  }
+`;
